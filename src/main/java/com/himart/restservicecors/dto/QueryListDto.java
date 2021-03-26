19870998f7159378e64ff8fef91213bffe713f54
jeon3029 <jeon3029@naver.com>
2021-03-26 18:07:14 +0900

    add lombok

---
+++ src/main/java/com/himart/restservicecors/dto/QueryListDto.java
@@ -0,0 +1,14 @@
+package com.himart.restservicecors.dto;
+
+import lombok.Data;
+
+ @Data
+public class QueryListDto {
+	int q_code;
+	String title;
+	String query;
+	public QueryListDto() {
+		
+	}
+	
+}
